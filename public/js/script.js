d3bcce44a338848e9c7d59970f94533ebf9bdb9b
Keith Kovach <kovachks90@gmail.com>
2017-10-18 18:17:59 -0400

    scroll links work flawlessly

--- public/js/script.js
+++ public/js/script.js
@@ -6,26 +6,26 @@ $(document).ready(function(){
       if (this.hash !== "") {
         // Prevent default anchor click behavior
         event.preventDefault();
-        console.log(this.hash)
         // Store hash
         var hash = this.hash;
   
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
-          scrollTop: $(hash).offset().top -50
+          scrollTop: $(hash).offset().top -125
         }, 800, function(){
      
           // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
+          // window.location.hash = hash;
         });
-      } // End if
+      }; // End if
     });
   });
+
+//Functiion for scrolling and fixing the navbar and animation for the header
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 451) {
-        console.log("Nav should be set to fixed")
     $(".nav").addClass('navFixed');
     $(".bottomBorderHidden").addClass("bottomBorderFixed")
     $(".profilePicMin").switchClass("profilePicMin", "profilePicMinShown", 1000)
@@ -38,5 +38,5 @@ $(window).scroll(function() {
         $(".profilePicMinShown").addClass("profilePicMin").removeClass("profilePicMinShown")
         $(".nameFixedMinShown").addClass("nameFixedMin").removeClass("nameFixedMinShown")
         $(".aboutMe").removeClass("aboutMeMargin")
-    }
-})
+    };
+});

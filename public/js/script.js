b9bf081b0ae8fdd2433a7781d4cd5cbf1b17f794
Keith Kovach <kovachks90@gmail.com>
2017-10-18 19:10:35 -0400

    adding comments to jquery

--- public/js/script.js
+++ public/js/script.js
@@ -25,18 +25,32 @@ $(document).ready(function(){
 //Functiion for scrolling and fixing the navbar and animation for the header
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
+
+    //if user scrolls to the top of the hidden portion of the fixed elements
     if (scroll >= 451) {
+
+    //then fix the navbar
     $(".nav").addClass('navFixed');
+
+    //instantly display fixed nav bar
     $(".bottomBorderHidden").addClass("bottomBorderFixed")
+
+    //1 second animation for smaller portrait
     $(".profilePicMin").switchClass("profilePicMin", "profilePicMinShown", 1000)
-    $(".nameFixedMin").switchClass("nameFixedMin", "nameFixedMinShown", 1000)    
-    $(".aboutMe").addClass("aboutMeMargin")
+
+    //1 second animation for fixed name
+    $(".nameFixedMin").switchClass("nameFixedMin", "nameFixedMinShown", 1000)
+    
+    //Section to give some margin for the newly displayed fixed divs
+    $("#aboutMe").addClass("aboutMeMargin")
     }
     else {
+      
+        //Otherwise restore to default
         $(".nav").removeClass("navFixed")
         $(".bottomBorderHidden").removeClass("bottomBorderFixed")
         $(".profilePicMinShown").addClass("profilePicMin").removeClass("profilePicMinShown")
         $(".nameFixedMinShown").addClass("nameFixedMin").removeClass("nameFixedMinShown")
-        $(".aboutMe").removeClass("aboutMeMargin")
+        $("#aboutMe").removeClass("aboutMeMargin")
     };
 });

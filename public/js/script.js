0595f8e8b008facbfc17b02b3fdf9cae5e7dda06
Keith Kovach <kovachks90@gmail.com>
2017-10-21 13:56:14 -0400

    file transfer

--- public/js/script.js
+++ public/js/script.js
@@ -15,8 +15,6 @@ $(document).ready(function(){
           scrollTop: $(hash).offset().top -125
         }, 800, function(){
      
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          // window.location.hash = hash;
         });
       }; // End if
     });

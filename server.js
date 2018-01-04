9c0e6759c63f13e51f4790604ea5bd40a892809d
Keith Kovach <kovachks90@gmail.com>
2018-01-03 22:07:14 -0500

    updated

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var config = require("./config.js")
 var $ = require('jquery');
 var nodemailer = require('nodemailer');
 var sgMail = require('@sendgrid/mail');
+var sq = require("sendgrid")(process.env.SENDGRID_API_KEY);
 
 var app = express();
 var PORT = process.env.PORT || 8000;
@@ -23,19 +24,41 @@ app.get("/", function(req, res) {
 })
 
 app.get('/send',function(req,res){
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-    to: 'kovachks90@gmail.com',
-    from: 'kovachkeithportfolio',
-    subject: 'Message From Portfolio',
-    text: "Entered Email: " + req.query.from + "  Entered Name: " + req.query.name + "  Email Text: " + req.query.text
-    };
-    sgMail.send(msg, function(error, response) {
-        if (error) {
-            console.log(error);
-            res.end("error")
-        }
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: {
+            personalizations: [
+                {
+                    to: [
+                        {
+                            email: 'kovachks90@gmail.com',
+                        },
+                    ],
+                    subject: "Message From Portfolio",
+                },
+        ],
+        from: {
+            email: 'kovachkeithportfolio@portfolio.com'
+        },
+        content: [
+            {
+                type: 'text/plain',
+                value: "Entered Email: " + req.query.from + "  Entered Name: " + req.query.name + "  Email Text: " + req.query.text
+            },
+        ],
+        },
     });
+
+    sg.API(request)
+    .then(response => {
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+    })
+    .catch(error => {
+        console.log(error.response.statusCode);
+    })
 });
 
 app.listen(PORT, function() {
